e661e3bef9f5e1756ede5048770ba6d57d42b7c4
htk007 <hasankaya921@hotmail.com>
2023-05-11 22:40:12 +0300

    add Constants

---
+++ app/src/main/java/com/heka/isimsozlukapp/util/Constants.java
@@ -0,0 +1,11 @@
+package com.heka.isimsozlukapp.util;
+
+public class Constants {
+    public static final String BASE_URL =  "https://www.behindthename.com/api/";
+    public static final String LOOKUP_JSON_NAME="lookup.json";
+    public static final String RELATED_JSON_NAME="related.json";
+    public static final String QUERY_KEY_NAME="name";
+    public static final String QUERY_KEY_USAGE="usage";
+    public static final String QUERY_KEY_KEY="key";
+
+}
