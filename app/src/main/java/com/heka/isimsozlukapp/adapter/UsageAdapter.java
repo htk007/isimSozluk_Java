bfa5115178b75c46bad3fda2076a9f9a6236d379
htk007 <hasankaya921@hotmail.com>
2023-05-22 22:18:17 +0300

    add click event

--- app/src/main/java/com/heka/isimsozlukapp/adapter/UsageAdapter.java
+++ app/src/main/java/com/heka/isimsozlukapp/adapter/UsageAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,8 +17,8 @@ import com.heka.isimsozlukapp.model.Usage;
 import java.util.List;
 
 public class UsageAdapter extends RecyclerView.Adapter<UsageAdapter.UsageViewHolder> {
-private Context context;
-private List<Usage> usageList;
+private static Context context;
+protected static List<Usage> usageList;
 
     public UsageAdapter(Context context, List<Usage> usageList) {
         this.context = context;
@@ -54,6 +55,17 @@ private List<Usage> usageList;
         public UsageViewHolder(@NonNull View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.nameTextView);
+            nameTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION) {
+                        Usage clickedItem = usageList.get(position);
+                        String message = "Tıklanan öğe: " + clickedItem.getUsageFull();
+                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 }
